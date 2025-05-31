0bf3cddcf888d646937341665bb304c764849835
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2025-05-30 18:36:42 -0700

    feat: Add Netlify Identity widget script to _document.tsx and remove unused index.js file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <body>
         <Main />
